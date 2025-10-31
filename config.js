5e0e8111a59bd6c4d733bab90c33bf8941f3fb56
ender <aubinlef2005@gmail.com>
2025-10-31 01:47:15 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,43 @@
+const token = "MTE5MDI3MjcyMzIxODM0NTk4Nw.GVm38Z.mek32o_A7sUY9e1r-vPN9brDyj39EG26zJBmn4"; // Here, leave your token
+const appID = ""; // Here, leave your appID empty = default
+const type = ""; // Here, leave your type [PLAYING, LISTENING, WATCHING, STREAMING] empty = default
+const name = ""; // Here, leave your name empty = default
+const details = ""; // Here, leave your details empty = default
+const state = ""; // Here, leave your state empty = default
+const largeKey = ""; // Here, leave your large image empty = default
+const smallKey = ""; // Here, leave your small image empty = default 
+const button1 = ""; // Here, leave your text button 1 empty = default
+const button2 = ""; // Here, leave your text button 2 empty = default
+const url1 = ""; // Here, leave your url button 1 empty = default
+const url2 = ""; // Here, leave your url button 2 empty = default
+const startTime = "69:00"; // 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = { // Do not touch
+    token, // Do not touch
+    appID, // Do not touch
+    type, // Do not touch
+    name, // Do not touch
+    details, // Do not touch
+    state, // Do not touch
+    largeKey, // Do not touch
+    smallKey, // Do not touch
+    button1, // Do not touch
+    button2, // Do not touch
+    url1, // Do not touch
+    url2, // Do not touch
+    startTime, // Do not touch
+}
